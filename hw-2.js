0851ebe30b5f8da32e45cf1fbac0f3d0639eb21d
AsiyatYun <pianoowl@icloud.com>
2024-10-05 23:44:24 +0300

    Добавила JS файл

---
+++ hw-2.js
@@ -0,0 +1,47 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+const iphone = Number(2007);
+alert(iphone);
+
+const creator = String("Brendan Eich");
+alert(creator);
+
+let ten = 10;
+let two = 2;
+alert(ten + two);
+alert(ten - two);
+alert(ten * two);
+alert(ten / two);
+
+let result = 2**5; 
+alert(result);
+
+a = 9;
+let b = 2;
+alert(a % b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = Number(prompt('Сколько Вам лет?'));
+alert(age);
+
+const user = {
+    name: String('Ab'),
+    age: Number(18),
+    isAdmin: Boolean(false),
+ };
+
+
+let guest = prompt('Ваше имя?');
+alert(`Привет, ${guest}!`);
+
