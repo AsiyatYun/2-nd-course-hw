dddf534ba3695853e096bb8f280f6822a4e86d0c
AsiyatYun <pianoowl@icloud.com>
2024-10-08 00:15:41 +0300

    Исправила ошибки

--- hw-2.js
+++ hw-2.js
@@ -1,47 +1,58 @@
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
 
-const iphone = Number(2007);
-alert(iphone);
+// const iphone = Number(2007);
+// alert(iphone);
 
-const creator = String("Brendan Eich");
-alert(creator);
+// const creator = String("Brendan Eich");
+// alert(creator);
 
-let ten = 10;
-let two = 2;
-alert(ten + two);
-alert(ten - two);
-alert(ten * two);
-alert(ten / two);
+// let num1 = 10;
+// let num2 = 2;
+// alert(num1 + num2);
+// alert(num1 - num2);
+// alert(num1 * num2);
+// alert(num1 / num2);
 
-let result = 2**5; 
-alert(result);
+// let result = 2**5; 
+// alert(result);
 
-a = 9;
-let b = 2;
-alert(a % b);
+// a = 9;
+// let b = 2;
+// alert(a % b);
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num += 1;
-num -= 1;
-alert(num);
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
 
-let age = Number(prompt('Сколько Вам лет?'));
-alert(age);
+// let age = Number(prompt('Сколько Вам лет?'));
+// alert(age);
 
-const user = {
-    name: String('Ab'),
-    age: Number(18),
-    isAdmin: Boolean(false),
- };
+// const user = {
+//     name: String('Ab'),
+//     age: Number(18),
+//     isAdmin: Boolean(false),
+//  };
 
 
-let guest = prompt('Ваше имя?');
-alert(`Привет, ${guest}!`);
+// let guest = prompt('Ваше имя?');
+// alert(`Привет, ${guest}!`);
+
+let number = Number(prompt("Загадайте число"));
+let number2 = number * 2;
+alert(`умножить на 2 = ${number2}`);
+let number3 = number2 + 10;
+alert(`прибавить 10 = ${number3}`);
+let number4 = number3 / 2;
+alert(`разделить на 2 = ${number4}`);
+let number5 =number4 - number;
+alert(`вычтем загаданное число = ${number5}`);
+alert('Ответ равен 5');
 
