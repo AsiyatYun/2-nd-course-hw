ad78cdb8394c50a263f5e040ff3eea690c883209
AsiyatYun <pianoowl@icloud.com>
2024-11-25 23:25:21 +0300

    Доделала игру

--- hw-8.js
+++ hw-8.js
@@ -11,10 +11,12 @@ console.log(compOption);
 let userAnsw = prompt ('Выберите оружие')
 
 if (userAnsw.toLowerCase() === compOption.toLowerCase()) {
-    alert(`Выбор соперника - ${compOption}. Ничья!`);
-    
-} else if ((userAnsw.toLowerCase() === 'камень' && compOption.toLowerCase() === 'ножницы') || (userAnsw.toLowerCase() === 'ножницы' && compOption.toLowerCase() === 'бумага') || (userAnsw.toLowerCase() === 'бумага' && compOption.toLowerCase() === 'камень')) {
+    alert(`Выбор соперника - ${compOption}. Ничья!`);    
+} 
+else if ((userAnsw.toLowerCase() === 'камень' && compOption.toLowerCase() === 'ножницы') || (userAnsw.toLowerCase() === 'ножницы' && compOption.toLowerCase() === 'бумага') || (userAnsw.toLowerCase() === 'бумага' && compOption.toLowerCase() === 'камень')) {
     alert(`Выбор соперника - ${compOption}. Поздравляю с победой!`);
+} else if ((userAnsw.toLowerCase() !== rockPaper[0].toLowerCase()) && (userAnsw.toLowerCase() !== rockPaper[1].toLowerCase()) && (userAnsw.toLowerCase() !== rockPaper[2].toLowerCase())) {
+    alert('Некорректное значение');    
 } else {
     alert(`Выбор соперника - ${compOption}. Вы проиграли!`);
 
@@ -24,94 +26,96 @@ if (userAnsw.toLowerCase() === compOption.toLowerCase()) {
 
 
 
-// // задание1
+// задание1
 
-// const people1 = [
-//     { name: 'Глеб', age: 29 },
-//     { name: 'Анна', age: 17 },
-//     { name: 'Олег', age: 7 },
-//     { name: 'Оксана', age: 47 }
-//  ];
+const people1 = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
  
 
 
-//  console.log(people1.sort((a, b) => a.age - b.age));
+ console.log(people1.sort((a, b) => a.age - b.age));
 
 
-// // задание2
+// задание2
 
 
-// function isPositive(num) {
-//     return num > 0;
-// }
+function isPositive(num) {
+    return num > 0;
+}
 
-// function isMale(arr) {
-//     return arr.gender === 'male' 
-// }
+function isMale(arr) {
+    return arr.gender === 'male' 
+}
 
-// function filter(arr, callback) {
-//     const newArr = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         if (callback(arr[i])) {
-//             newArr.push(arr[i]);
-//         };
-//     }
+function filter(arr, callback) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
     
-//     return newArr;
+    return newArr;
     
-// }
+}
 
-// console.log(filter([3, -4, 1, 9], isPositive));
+console.log(filter([3, -4, 1, 9], isPositive));
 
-// const people = [
-//    {name: 'Глеб', gender: 'male'},
-//    {name: 'Анна', gender: 'female'},
-//    {name: 'Олег', gender: 'male'},
-//    {name: 'Оксана', gender: 'female'}
-// ];
+const people = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
 
-// console.log(filter(people, isMale));
+console.log(filter(people, isMale));
 
-// // задание3
-
-
+// задание3
 
 
-// // let timerId = setInterval(() => {
-// //     let currentDate = new Date;
-// //     console.log(currentDate);
-// // }, 3000);
 
-// // setTimeout(() => { clearInterval(timerId); console.log('30 секунд прошло'); }, 33000);
 
-// // задание4
+let timerId = setInterval(() => {
+    let currentDate = new Date();
+    console.log(currentDate);
+}, 3000);
 
-// function delayForSecond1(callback) {
-//     setTimeout(() => {
-//         callback();  
-//     }, 1000);
+setTimeout(() => { clearInterval(timerId); console.log('30 секунд прошло'); }, 33000);
+
+// задание4
+
+function delayForSecond1(callback) {
+    setTimeout(() => {
+        callback();  
+    }, 1000);
    
-// }
+}
 
-// // delayForSecond1(function () {
-// //    console.log('Привет, Глеб!');
-// // })
+delayForSecond1(function () {
+   console.log('Привет, Глеб!');
+})
 
-// // задание5
+// задание5
 
 
-// function delayForSecond(cb) {
-//     setTimeout(() => {
-//         console.log('Прошла одна секунда');
-//         if(cb) {  cb(); }
-//     }, 1000)
-// }
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) { 
+             cb(); 
+            }
+    }, 1000)
+}
 
-// function sayHi (name) {
-//     console.log(`Привет, ${name}!`);
-// }
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
 
-// delayForSecond(() => sayHi('Глеб'))    
+delayForSecond(() => sayHi('Глеб'))    
 
 
 
